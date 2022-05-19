dbbc16c27e56eca79f7998672f0d686e70796d65
Murtaza Pakawala <mypaka52@gmail.com>
2022-05-19 18:37:13 +1000

    completing the nav bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,38 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import Link from "next/link";
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import { DiCssdeck } from "react-icons/di";
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+} from "./HeaderStyles";
 
-const Header = () =>  (
-  <div>
-    Header
-  </div>
+const Header = () => (
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{ display: "flex", alignItems: "center", color: "white" }}>
+          <DiCssdeck size="3rem" /> <span>Murtaza</span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <Link href="#about">
+        <NavLink>About</NavLink>
+      </Link>
+      <Link href="#projects">
+        <NavLink>Projects</NavLink>
+      </Link>
+      <Link href="#tech">
+        <NavLink>Technologies</NavLink>
+      </Link>
+    </Div2>
+  </Container>
 );
 
 export default Header;
