452993d167284fe8cfeee26c33168b549835e55a
Murtaza Pakawala <mypaka52@gmail.com>
2022-05-19 20:28:46 +1000

    completed with the project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React from "react";
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section id="projects" nopadding>
+    <SectionDivider />
+    <SectionTitle main> Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(({ id, image, title, description, tags, source }) => (
+        <BlogCard>
+          <Img src={image} />
+          <TitleContent>
+            <HeaderThree title>{title}</HeaderThree>
+            <hr />
+          </TitleContent>
+          <CardInfo>{description}</CardInfo>
+          <div>
+            <TitleContent>
+              <TagList>
+                {tags.map((tag, i) => (
+                  <Tag>{tag}</Tag>
+                ))}
+              </TagList>
+            </TitleContent>
+          </div>
+          <UtilityList>
+            <ExternalLinks href={source}>github repo</ExternalLinks>
+          </UtilityList>
+        </BlogCard>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,46 +1,49 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "My courses",
+    description:
+      "This is native android app where I made user authentication handled by firebase, main function of app was to sort students courses for the term",
+    image: "/images/6.png",
+    tags: ["Kotlin", "Firebase", "Retrofi"],
+    source: "https://github.com/MurtazaPakawala/Courses-app",
+
     id: 0,
   },
   {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "E-Commerce-cart",
+    description:
+      "In this react project I implemented cart where we could add the items, delete and other functionality which other e-commerce websites cart have",
+    tags: ["React", "JavaScript"],
+    source: "https://google.com",
+
     id: 1,
   },
   {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
-      tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "Chat App",
+    description:
+      "In this app I used and learned about firebase user authentication and built a basic chat application",
+    image: "/images/3.png",
+    tags: ["Kotlin", "Firebase"],
+    source: "https://google.com",
+
     id: 2,
   },
   {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "Yelp Clone",
+    description:
+      "In this android app I learned about the retrofit library for calling API and used the yelp API to find pizza restaurants near sydney",
+    image: "/images/5.png",
+    tags: ["Kotlin", "Retrofit"],
+    source: "https://github.com/MurtazaPakawala/Yelp-Clone",
+
     id: 3,
   },
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
 ];
